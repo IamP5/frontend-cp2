61cec5d08ceedbc506de119785a71b4fe8b36d23
Tuba <brunogabrielsd@hotmail.com>
2024-05-01 20:55:37 -0300

    fix: remove from wishlist

--- wishlist/main.js
+++ wishlist/main.js
@@ -27,7 +27,7 @@ function loadWishList() {
               id="wishlist-icon" 
               class="wine__button wine__button--wishlist ${isWishListed ? 'wine__button--wishlist-active' : ''}"
               src="./assets/icons/heart.svg" 
-              onclick="addToWishList(${wine.id});"
+              onclick="addToWishList(${wine.id}); loadWishList();"
             >
           </div>
         </section>
